9356332f2ff3c9b18e67c794c602fd647035cc55
Quang <quangtqag@gmail.com>
2017-05-11 14:47:14 +0700

    Add files via upload

---
+++ FlickrSearch/FlickrSearch/FlickrPhotoHeaderView.swift
@@ -0,0 +1,14 @@
+//
+//  FlickrPhotoHeaderView.swift
+//  FlickrSearch
+//
+//  Created by Quang Tran on 5/9/17.
+//  Copyright © 2017 Richard Turton. All rights reserved.
+//
+
+import UIKit
+
+class FlickrPhotoHeaderView: UICollectionReusableView {
+        
+  @IBOutlet weak var label: UILabel!
+}
