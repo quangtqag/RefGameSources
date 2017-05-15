bf01547d7414a93168e72b606a1b999dd09617f5
Quang <quangtqag@gmail.com>
2017-05-15 17:24:25 +0700

    Add files via upload

---
+++ ExpandingCollectionView/ExpandingCollectionView/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Ultravisual
+//
+//  Created by Mic Pringle on 20/03/2015.
+//  Copyright (c) 2015 Razeware LLC. All rights reserved.
+//
+//  https://www.raywenderlich.com/99087/swift-expanding-cells-ios-collection-views
+//
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    return true
+  }
+
+}
+
---
+++ ExpandingCollectionView/ExpandingCollectionView/InspirationCell.swift
@@ -0,0 +1,52 @@
+//
+//  TutorialCell.swift
+//  RWDevCon
+//
+//  Created by Mic Pringle on 27/02/2015.
+//  Copyright (c) 2015 Ray Wenderlich. All rights reserved.
+//
+
+import UIKit
+
+class InspirationCell: UICollectionViewCell {
+  
+  @IBOutlet fileprivate weak var imageView: UIImageView!
+  @IBOutlet fileprivate weak var imageCoverView: UIView!
+  @IBOutlet private weak var titleLabel: UILabel!
+  @IBOutlet private weak var timeAndRoomLabel: UILabel!
+  @IBOutlet private weak var speakerLabel: UILabel!
+
+
+  var inspiration: Inspiration? {
+    didSet {
+      if let inspiration = inspiration {
+        imageView.image = inspiration.backgroundImage
+        titleLabel.text = inspiration.title
+        timeAndRoomLabel.text = inspiration.roomAndTime
+        speakerLabel.text = inspiration.speaker
+      }
+    }
+  }
+  
+  override func apply(_ layoutAttributes: UICollectionViewLayoutAttributes) {
+    super.apply(layoutAttributes)
+    
+    // 1
+    let standardHeight = UltravisualLayoutConstants.Cell.standardHeight
+    let featuredHeight = UltravisualLayoutConstants.Cell.featuredHeight
+    
+    // 2
+    let delta = 1 - ((featuredHeight - frame.height) / (featuredHeight - standardHeight))
+    
+    // 3
+    let minAlpha: CGFloat = 0.3
+    let maxAlpha: CGFloat = 0.75
+    imageCoverView.alpha = maxAlpha - (delta * (maxAlpha - minAlpha))
+    
+    let scale = max(delta, 0.5)
+    titleLabel.transform = CGAffineTransform(scaleX: scale, y: scale)
+    
+    timeAndRoomLabel.alpha = delta
+    speakerLabel.alpha = delta
+  }
+}
